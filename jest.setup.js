747863c65420874b28ca09a0743badcf14be6d85
kynguyen28 <truongkynguyen11@gmail.com>
2025-10-28 14:57:13 +0700

    index2

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,3 @@
-import "@testing-library/jest-dom";
-import { screen } from "@testing-library/dom";
-import { loadQuestions } from "../script.js";
-import React, { useState } from "react";
-import NavBar from "./NavBar";
-import QuestionCard from "./QuestionCard";
-import questions from "./data";
+// jest.setup.js
+require("@testing-library/jest-dom");
+
